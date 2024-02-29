aa48afeb82f14e7beac83c246d6d0ddb1bc80401
Piyush Pardeshi <piyushp0512@gmail.com>
2024-03-01 01:50:02 +0530

    Home layout responsive improved

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -25,9 +25,9 @@ const Header = () => {
   return (
     <header className="z-10 w-fit h-fit bg-bgLight dark:bg-bgDark">
       {/* // Computer header */}
-      <div className="p-6 xl:max-w-[350px] xl:min-w-[350px] w-fit hidden md:flex h-screen border-r-2 border-solid border-gray-300 flex-col justify-between">
+      <div className="p-6 md:max-w-[270px] md:min-w-[270px] lg:max-w-[350px] lg:min-w-[350px] w-fit hidden sm:flex h-screen border-r-2 border-solid border-gray-300 flex-col justify-between">
         <div>
-          <div className="flex items-center justify-center xl:justify-start">
+          <div className="flex items-center justify-center sm:justify-center md:justify-start">
             <img src={vicharLogo} className="w-[40px] mb-[50px]" />
           </div>
           {headerData.map((item) => {
@@ -35,13 +35,10 @@ const Header = () => {
             return (
               <div
                 key={item.id}
-                className="flex items-center justify-center xl:justify-start gap-5 mb-5"
+                className="flex items-center justify-center sm:justify-center md:justify-start gap-5 mb-5"
               >
-                <img
-                  src={IconComponent}
-                  className="w-[25px] h-[25px] stroke-cyan-500 "
-                />
-                <h2 className="text-[18px] text-textLight dark:text-textDark hidden xl:block">
+                <img src={IconComponent} className="w-[25px] h-[25px] " />
+                <h2 className="text-[18px] text-textLight dark:text-textDark hidden md:block">
                   {item.title}
                 </h2>
               </div>
@@ -49,9 +46,9 @@ const Header = () => {
           })}
         </div>
         <div className="flex flex-col gap-5">
-          <button className="bg-primaryBlue hover:bg-[#307baa] transition-colors w-fit xl:w-full text-white p-3 rounded-full ">
-            <span className="hidden xl:block">Post</span>
-            <img src={AddVichar} className="xl:hidden block w-[25px]" />
+          <button className="bg-primaryBlue hover:bg-[#307baa] transition-colors w-fit sm:w-full text-white p-3 rounded-full ">
+            <span className="hidden md:block">Post</span>
+            <img src={AddVichar} className="md:hidden block w-[25px]" />
           </button>
           <div className="flex justify-between items-center gap-2 cursor-pointer rounded-full hover:bg-gray-300 dark:hover:bg-[#2b2b2b] transition-colors p-0 xl:px-4 xl:py-3">
             <div className="flex justify-center items-center gap-3">
@@ -62,24 +59,24 @@ const Header = () => {
                   alt=""
                 />
               </figure>
-              <div className="hidden xl:block">
-                <p className="font-bold text-[1em] text-textLight dark:text-textDark">
+              <div className="hidden md:block">
+                <p className="font-bold text-[1em] md:text-[14px] text-textLight dark:text-textDark">
                   Piyush Pardeshi
                 </p>
-                <p className="text-[14px] text-textLight dark:text-textDark">
+                <p className="text-[14px] md:text-[12px] text-textLight dark:text-textDark">
                   @pixi
                 </p>
               </div>
             </div>
             <BsThreeDots
               size={20}
-              className="text-textLight dark:text-textDark hidden xl:block  "
+              className="text-textLight dark:text-textDark hidden md:block  "
             />
           </div>
         </div>
       </div>
       {/* // Mobile header */}
-      <div className="md:hidden w-full h-fit p-5 fixed top-0 bg-bgLight dark:bg-bgDark flex justify-center items-center">
+      <div className="sm:hidden w-full h-fit p-5 fixed top-0 bg-bgLight dark:bg-bgDark flex justify-center items-center">
         <div className="w-full max-w-md flex flex-row justify-between ">
           <figure>
             <img
--- frontend/src/components/Header/Navbar.jsx
+++ frontend/src/components/Header/Navbar.jsx
@@ -18,12 +18,12 @@ const iconComponents = {
 const Navbar = () => {
   const NavbarItems = NavbarData;
   return (
-    <div className="flex md:hidden items-center justify-center fixed bottom-0 z-10 w-full h-fit bg-bgLight dark:bg-bgDark">
+    <div className="flex sm:hidden items-center justify-center fixed bottom-0 z-10 w-full h-fit bg-bgLight dark:bg-bgDark">
       <div className="w-full p-5 mx-5 flex justify-between max-w-md">
         {NavbarItems.map((item, index) => {
           const IconComponent = iconComponents[item.icon];
           return (
-            <div>
+            <div key={index}>
               <img
                 src={IconComponent}
                 className="w-[25px] h-[25px] stroke-cyan-500 "
